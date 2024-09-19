d6aa5d7fd3e32332ae520b7929ad0b4d7cae20f9
Vadhir <lomuner@gmail.com>
2024-09-19 20:34:04 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,51 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = [
+  "Un malilloso",
+  "Una viejita",
+  "Un viejito",
+  "Una señora embarazada",
+  "Un vendedor ambulante"
+];
+let action = [
+  "me pidió ayuda",
+  "me preguntó una dirección",
+  "me solicitó apoyo"
+];
+let what = [
+  "para ir al hospital",
+  "para llegar a la farmacia",
+  "para ir al banco",
+  "para llegar a la iglesia"
+];
+let when = [
+  "antes de llegar al trabajo",
+  "justo cuando salí de casa",
+  "mientras trotaba por la mañana",
+  "en mi hora de comida",
+  "al ir a la escuela"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
